b95062a95c751033609e36478fbd328a19d38e3f
Star Brilliant <coder@poorlab.com>
2025-02-08 06:45:55 +0000

    Add environment variable DNSMASQ_LISTEN_ADDR

--- docker/entrypoint-better-ping.sh
+++ docker/entrypoint-better-ping.sh
@@ -2,6 +2,6 @@
 set -e
 
 install -Dm0644 /etc/resolv.conf /run/dnsmasq/resolv.conf
-dnsmasq -i lo -a "$DNSMASQ_LISTEN_ADDR" -r /run/dnsmasq/resolv.conf -c 1000
+dnsmasq -i lo -a "$DNSMASQ_LISTEN_ADDR" -z -r /run/dnsmasq/resolv.conf -c 1000
 echo "nameserver $DNSMASQ_LISTEN_ADDR" >/etc/resolv.conf
 exec /entrypoint.sh "$@"
